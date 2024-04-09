d6a687cb599f2fba937f9c28bd36f39455c661f1
sonalikari <sonali211103@gmail.com>
2024-04-09 12:03:59 +0530

    fixed comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,6 @@ exports.registerUser = async (req, res) => {
         if (password !== confirmPassword) {
             return res.status(400).json({ error: 'Passwords do not match' });
         }
-
         const hashedPassword = await bcrypt.hash(password, 8); 
 
         const newUser = new User({
@@ -43,14 +42,14 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.getUserData = async (req, res) => {
-    
-    const accessToken = req.headers['access_token'];
-
-    const user = await User.findById(accessToken);
-
-    if (!user) {
-        return res.status(400).json({ error: 'User not found or invalid access token' });
+    try {
+        const accessToken = req.headers['access_token'];
+        const user = await User.findById(accessToken);
+        if (!user) {
+            return res.status(400).json({ error: 'User not found or invalid access token' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'An error while fetching user data' });
     }
-
-    res.json(user);
 };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,18 @@
 const User = require('../models/user');
-
 exports.validateAccessToken = async (req, res, next) => {
-
     const accessToken = req.headers['access_token'];
 
-    const user = await User.findById(accessToken);
-
-    if (!user) {
-        return res.status(400).json({ error: 'User not found or invalid access token' });
+    if (!accessToken) {
+        return res.status(400).json({ error: 'Access token is required' });
     }
-    req.user = user;
-    next();
+    try {
+        const user = await User.findById(accessToken);
+        if (!user) {
+            return res.status(400).json({ error: 'User not found or invalid access token' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(500).json({ error: 'Error while validating access token' });
+    }
 };
